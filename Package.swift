525579ff86245ba9b89014ef7a58711a386419e4
Steve Madsen <steve@lightyearsoftware.com>
2023-02-10 10:47:00 -0500

    Update Nimble to 11.2.1

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["AsyncRequest"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Quick/Nimble", from: "10.0.0"),
+        .package(url: "https://github.com/Quick/Nimble", from: "11.0.0"),
         .package(url: "https://github.com/AliSoftware/OHHTTPStubs", from: "9.1.0")
     ],
     targets: [
